85371cc1ff85811aad12fffc9244b29e4b32f095
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-05-21 23:25:11 +0330

    create post model and change queryset in PostListView

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    cover = models.ImageField(upload_to='posts/')
+
+    status = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.views import generic
-
+from . models import Post
 # Create your views here.
 
-class PostListView(generic.TemplateView):
+class PostListView(generic.ListView):
+    queryset = Post.objects.filter(status=True)
     template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
