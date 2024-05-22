a5fe36ab0315569cf2393b99dbd1cfb49adf7640
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-05-22 13:11:10 +0330

    show categories in post_list.html & optimization the querysets in posts.views

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,25 @@
 from django.views import generic
-from . models import Post, Tag
+from . models import Post, Tag, Category
 # Create your views here.
 
 class PostListView(generic.ListView):
-    queryset = Post.objects.filter(status=True)
+    queryset = Post.objects.filter(status=True).defer(
+                                                    'description',
+                                                    'datetime_modified',
+                                                    'status',
+                                                )
     template_name = 'posts/post_list.html'
     context_object_name = 'posts'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tags'] = Tag.objects.all()
+        context['tags'] = Tag.objects.all().defer(
+                                            'datetime_created', 
+                                            'datetime_modified',
+                                          )
+        context['categories'] = Category.objects.all().defer(
+                                                        'datetime_created',
+                                                        'datetime_modified',
+                                                    )
 
         return context
