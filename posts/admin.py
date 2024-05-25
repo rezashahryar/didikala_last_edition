0446e34da9df4ef703ccbb40ea9415f263e40201
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-05-25 12:09:17 +0330

    change queryset for field author in PostAdmin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Tag, Category
+from core.models import User
 # Register your models here.
 
 @admin.register(Post)
@@ -8,6 +9,11 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('description',)
     list_display = ['title', 'status']
     prepopulated_fields = {'slug': ('title',)}
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'author':
+            kwargs['queryset'] = User.objects.filter(is_superuser=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
     
 
 @admin.register(Tag)
