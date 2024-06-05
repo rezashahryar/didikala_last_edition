7d0cf04c093dec9f41f7fc6915603884e027aea5
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-06-05 21:30:44 +0330

    display profile page & template it

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def profile(request):
+
+    return render(request, 'profiles/profile.html')
+
 
 def profile_edit_view(request):
 
